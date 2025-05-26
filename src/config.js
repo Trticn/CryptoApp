a2f6cffb00b96acab4632780cd2f75307e600756
Nikola Trtic <nikolatrtic02@gmail.com>
2025-05-26 12:56:57 +0200

    Changing DB link

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const DB_LINK = 'https://cryptoappbackend.onrender.com';
+// export const DB_LINK = 'https://cryptoappbackend.onrender.com';
+export const DB_LINK = 'https://json-backend-production.up.railway.app';
 export const API_LINK = 'https://api.coingecko.com/api/v3';
