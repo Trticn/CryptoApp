1712de251053238e7cc122d4ed06eb05a760ec01
Nikola Trtic <nikolatrtic02@gmail.com>
2025-07-21 15:36:25 +0200

    Connecting with backend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const DB_LINK = 'https://cryptoappbackend.onrender.com';
-export const DB_LINK = 'http://localhost:1234'
+export const DB_LINK = 'https://cryptoappbackend.onrender.com'
 export const API_LINK = 'https://api.coingecko.com/api/v3';
